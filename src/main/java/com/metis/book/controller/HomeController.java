612cb14168024ea99e3b463ab23fbdcccb45d4e3
kiet2709 <tuankietlenguyen2709@gmail.com>
2022-10-05 21:20:34 +0700

    modified for testing in 5/10

--- src/main/java/com/metis/book/controller/HomeController.java
+++ src/main/java/com/metis/book/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 @RequestMapping("/metis/home")
 public class HomeController {
@@ -11,6 +12,6 @@ public class HomeController {
 	
 	@GetMapping
 	public String home() {
-		return "service";
+		return "admin/order.html";
 	}
 }
