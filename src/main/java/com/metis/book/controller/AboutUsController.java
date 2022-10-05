bde792175f308a6bf86c9aa722b8fdeac2142946
khai <duckhailinux@gmail.com>
2022-10-06 00:28:57 +0700

    Add about.html

---
+++ src/main/java/com/metis/book/controller/AboutUsController.java
@@ -0,0 +1,15 @@
+package com.metis.book.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/metis/about")
+public class AboutUsController {
+
+	@GetMapping
+	public String About() {
+		return "client/about.html";
+	}
+}
